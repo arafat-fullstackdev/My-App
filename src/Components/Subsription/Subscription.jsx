e45afb2eb259867c982af83722f51662d32ca5ea
Arafat Patwary <farabipsk@gmail.com>
2024-06-29 22:05:25 +0600

    Update subscription component

--- src/Components/Subsription/Subscription.jsx
+++ src/Components/Subsription/Subscription.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Subscription = () => {
+
+    let date= (new Date('2024','06','29'));
+    let title = "Monthly Subscription";
+    let amount = '124.23';
   return (
-    <div>Subscription</div>
+    <div className='Subscription'>
+        <h1 className='date'>{date.toISOString}</h1>
+        <h2>{title}</h2>
+        <p>{amount}</p>
+    </div>
   )
 }
 
