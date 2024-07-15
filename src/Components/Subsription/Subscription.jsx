f1b5bea911b9c210e15c31f052f8fbea95dda95d
Arafat Patwary <farabipsk@gmail.com>
2024-07-15 09:22:15 +0600

    update date

--- src/Components/Subsription/Subscription.jsx
+++ src/Components/Subsription/Subscription.jsx
@@ -1,10 +1,15 @@
 import "./Subscription.css";
 
  function Subscription(props){
+     const day=props.date.toLocaleString('default',{day:'2-digit'});
+     const month=props.date.toLocaleString('default',{month:'long'});
+     const year=props.year.getFullYear();
 
        return(
         <div className="subscription">
-          <div>{props.date.toISOString()}</div>
+          <div>{day}</div>
+          <div>{month}</div>
+          <div>{year}</div>
           <h2 className="subscription_title">{props.title}</h2>
           <div className="subscription_price">{props.amount}</div>
 
